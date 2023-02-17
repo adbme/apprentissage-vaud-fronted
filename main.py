8752842d775f7362294621b078e0c61ee40d5ec6
FY4ll <fadyallemann@gmail.com>
2023-02-17 10:36:46 +0100

    Import the HTML content with Requests

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Télécharger la page Web
+url = "https://www.orientation.ch/dyn/show/2930?lang=fr&Idx=0&OrderBy=1&Order=0&PostBackOrder=0&postBack=true&CountResult=0&Total_Idx=0&CounterSearch=2&UrlAjaxWebSearch=%2FLenaWeb%2FAjaxWebSearch&getTotal=False&isBlankState=True&prof_=88613.1&fakelocalityremember=&LocName=&LocId=&Area=10&cty_=VD"
+response = requests.get(url)
+
+# Créer l'objet BeautifulSoup à partir du contenu de la page téléchargée
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Extraire tous les éléments div contenant une ID qui commence par "anchore"
+elements = soup.find_all("div", {"id": lambda x: x and x.startswith("anchor")})
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-,zt
