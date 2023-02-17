aeeaac4f35fc6ee8082ccfeedd053e0572f7bc81
FY4ll <fadyallemann@gmail.com>
2023-02-17 14:30:28 +0100

    transfer a json to the databse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
-
 import requests
 from lxml import etree
+from datetime import datetime
 
 # Télécharger la page Web
 url = "https://www.orientation.ch/dyn/show/2930?lang=fr&Idx=0&OrderBy=1&Order=0&PostBackOrder=0&postBack=true&CountResult=0&Total_Idx=0&CounterSearch=2&UrlAjaxWebSearch=%2FLenaWeb%2FAjaxWebSearch&getTotal=False&isBlankState=True&prof_=88613.1&fakelocalityremember=&LocName=&LocId=&Area=10&cty_=VD"
@@ -25,15 +25,28 @@ for div in resultbox:
     places = div.findall('div')[5].text.split()[0]
     lastUpdate = div.findall('div')[6].text
     origin = 'orientation.ch'
+    originID = div.findall('div')[0].attrib["data-id"]
+    date_obj = datetime.strptime(lastUpdate, '%d.%m.%y')
+    formatted_date = date_obj.strftime('%Y-%m-%d')
+    print(formatted_date)
+    print(originID)
     apprenticeship = {
+        'data':{
         'company': company,
         'locality': locality,
         'language': language,
         'year': year,
         'places': places,
-        'lastUpdate': lastUpdate,
-        'origin': origin
+        'lastUpdate': formatted_date,
+        'origin': origin,
+        'originID': int(originID)
+        }
     }
     print(apprenticeship)
-    json_data = json.dumps(apprenticeship)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post("", headers=headers, data=json.dumps(apprenticeship))
+    print(response.json())
+
 
